def6987f7b3d43a60d37bdb306a0cf8b4a710c32
stevenneuhaus <newbais516@gmail.com>
2018-02-16 17:05:24 -0500

    added import data code.

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
 # test. hello world!
 
 #
+
+##Import libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+from scipy import optimize
+
+importdata = np.genfromtxt('IS400.csv',delimiter=',');  #read in data that i output from matlab. input format has each
+                                                        #of the 16 Temperature data sets stacked vertically.
+npoints = 101; #each T has 101 points
+IS400 = np.zeros((16,npoints,5)) #Initialize new 3d array to hold data. note: first index is what we would traditionally
+                            #think of as the last index. it makes sense though when you think hierarchically,
+                            #i.e. main list has 16 elements each of which has 101 smaller elements containing 5 elemnts.
+for i in range(0,16):
+    IS400[i,:,:]=importdata[(npoints*i):(npoints*(i+1)) , :] #slice the data into subarrays
